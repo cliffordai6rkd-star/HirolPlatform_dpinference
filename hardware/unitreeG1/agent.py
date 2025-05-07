c4048a8899a02238ec14a489bb6e7f37a6ae8711
hph <kjustdoitno1@gmail.com>
2025-05-07 14:45:20 +0800

    add mainboard/bms/odom support

--- hardware/unitreeG1/agent.py
+++ hardware/unitreeG1/agent.py
@@ -8,7 +8,7 @@ from hardware.unitreeG1.camera import Camera
 
 from hardware.base.robot import Robot
 
-from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowCmd_, LowState_
+from unitree_sdk2py.idl.unitree_hg.msg.dds_ import LowCmd_, LowState_, MainBoardState_, BmsState_
 from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelPublisher, ChannelFactoryInitialize
 from unitree_sdk2py.comm.motion_switcher.motion_switcher_client import MotionSwitcherClient
 from unitree_sdk2py.idl.default import unitree_hg_msg_dds__LowCmd_
@@ -69,10 +69,34 @@ class Agent(Robot):
         self.lowstate_subscriber_ = ChannelSubscriber("rt/lowstate", LowState_)
         self.lowstate_subscriber_.Init(self.LowStateHandler, 10)
 
+        self.mainboardstate_subscriber_ = ChannelSubscriber("rt/lf/mainboardstate", MainBoardState_)
+        self.mainboardstate_subscriber_.Init(self.MainBoardStateHandler, 10)
+
+        self.bms_subscriber_ = ChannelSubscriber("rt/lf/bmsstate", BmsState_)
+        self.bms_subscriber_.Init(self.BMSStateHandler, 10)
+
         # https://support.unitree.com/home/zh/G1_developer/odometer_service_interface
         # self.odom_sub_ = ChannelSubscriber("rt/odommodestate", SportModeState_)
-        # self.odom_sub_ = ChannelSubscriber("rt/lf/odommodestate", SportModeState_)
-        # self.odom_sub_.Init(self.HighStateHandler, 10)
+        self.odom_sub_ = ChannelSubscriber("rt/lf/odommodestate", SportModeState_)
+        self.odom_sub_.Init(self.HighStateHandler, 10)
+
+        # G1 常用TOPIC 列表
+        # TopicName 	Idl 	Info
+        # rt/dex3/left/state 	hg/idl/HandState_.idl 	获取左灵巧手反馈状态-低频模式
+        # rt/lf/dex3/left/state 	hg/idl/HandState_.idl 	获取左灵巧手反馈状态
+        # rt/dex3/left/cmd 	hg/idl/HandCmd_.idl 	控制左灵巧手
+        # rt/dex3/right/state 	hg/idl/HandState_.idl 	获取左灵巧手反馈状态
+        # rt/lf/dex3/right/state 	hg/idl/HandState_.idl 	获取右灵巧手反馈状态-低频模式
+        # rt/dex3/right/cmd 	hg/idl/HandCmd_.idl 	控制右灵巧手
+        # rt/lf/mainboardstate 	hg/idl/MainBoardState_.idl 	获取主板反馈信息
+        # rt/lowstate 	hg/idl/LowState_.idl 	获取底层反馈信息(IMU、电机等)
+        # rt/lf/lowstate 	hg/idl/LowState_.idl 	获取底层反馈信息(IMU、电机等)-低频模式
+        # rt/lowcmd 	hg/idl/LowCmd_.idl 	底层控制命令
+        # rt/lf/bmsstate 	hg/idl/BmsState_.idl 	获取电池反馈数据
+        # rt/odommodestate 	hg/idl/IMUState_.idl 	获取里程计信息
+        # rt/lf/odommodestate 	hg/idl/IMUState_.idl 	获取里程计信息-低频模式
+        # rt/lf/secondary_imu 	/hg/IMUState_.idl 	获取机身IMU数据-低频模式
+        # rt/secondary_imu 	/hg/IMUState_.idl 	获取机身IMU数据
 
         self.msc = MotionSwitcherClient()
         self.msc.SetTimeout(5.0)
@@ -102,6 +126,8 @@ class Agent(Robot):
 
         self.camera = Camera(config=config['camera'])
 
+        #TODO: add livox support
+
     def CameraCapture(self):
         return self.camera.capture()
 
@@ -118,11 +144,11 @@ class Agent(Robot):
             self.audio_client.TtsMaker(f"音量获取失败",0)
     
     def HighStateHandler(self, msg: SportModeState_):
-        log.info(f"Position: {msg.position}")
-        log.info(f"Velocity: {msg.velocity}")
-        log.info(f"Yaw velocity: {msg.yaw_speed}")
-        log.info(f"Foot position in body frame: {msg.foot_position_body}")
-        log.info(f"Foot velocity in body frame: {msg.foot_speed_body}")
+        log.debug(f"Position: {msg.position}")
+        log.debug(f"Velocity: {msg.velocity}")
+        log.debug(f"Yaw velocity: {msg.yaw_speed}")
+        log.debug(f"Foot position in body frame: {msg.foot_position_body}")
+        log.debug(f"Foot velocity in body frame: {msg.foot_speed_body}")
         
     def LowStateHandler(self, msg: LowState_):
         self.low_state = msg
@@ -130,6 +156,12 @@ class Agent(Robot):
         if self.update_mode_machine_ == False:
             self.mode_machine_ = self.low_state.mode_machine
             self.update_mode_machine_ = True
+
+    def MainBoardStateHandler(self, msg:MainBoardState_):
+        log.debug(f"Main Board State: {msg}")
+
+    def BMSStateHandler(self, msg: BmsState_):
+        log.debug(f"Battery State: {msg}")
 
     def print_state(self):
         log.info(f"IMU State: {self.low_state.imu_state}")
